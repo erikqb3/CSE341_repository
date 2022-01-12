89a15f29e8ce112a440f718dc43dbbf8f34aa284
erikqb3 <erikqb3@gmail.com>
2022-01-12 09:26:17 -0700

    Even more installations

--- ExpressJS_videos/WorkBench.js
+++ ExpressJS_videos/WorkBench.js
@@ -8,23 +8,31 @@ const app = express();
 //   console.log("In the middleware!");
 //   next(); //need next() in order to jumpt to next middleware (app.use)
 // });
-app.use('/', (req, res, next) => {
-  console.log("this always runs");
-  next();
-})
+// app.use('/', (req, res, next) => {
+//   console.log("this always runs");
+//   next();
+// })
+
+app.use()
 
 app.use(('/add-product', (req, res, next) => { //"/add-product" comes befre '/' because we won't be calling next(), thus preventing the other middle ware from happening, this is kind of a weird,unspoken,based on logic and cascading code "if statement"
   console.log("In another middleware!");
-  res.send('<h1>Add Product Page</h1>') //better than res.write
+  // res.send('<h1>Add Product Page</h1>') //better than res.write
+  res. send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
   // next();
 
 }));
 
-app.use(('/', (req, res, next) => {
+app.use('/product', (req,res,next) => {
+  console.log(req.body);
+  res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
   console.log("In another middleware!");
   res.send('<h1>Hellow from Express</h1>') //better than res.write
 
-}));
+});
 
 // const server = http.createServer(app);
 // server.listen(3000);
