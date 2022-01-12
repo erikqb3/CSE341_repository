583c526b3c2d3d57b3d0f0cef1bcf534d0d04fdf
erikqb3 <erikqb3@gmail.com>
2022-01-11 18:21:23 -0700

    This is kind of getting out of hand. Need to do something about all these installations

--- ExpressJS_videos/WorkBench.js
+++ ExpressJS_videos/WorkBench.js
@@ -1,9 +1,32 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+// app.use((req, res, next) => {
+//   console.log("In the middleware!");
+//   next(); //need next() in order to jumpt to next middleware (app.use)
+// });
+app.use('/', (req, res, next) => {
+  console.log("this always runs");
+  next();
+})
 
-server.listen(5000);
+app.use(('/add-product', (req, res, next) => { //"/add-product" comes befre '/' because we won't be calling next(), thus preventing the other middle ware from happening, this is kind of a weird,unspoken,based on logic and cascading code "if statement"
+  console.log("In another middleware!");
+  res.send('<h1>Add Product Page</h1>') //better than res.write
+  // next();
+
+}));
+
+app.use(('/', (req, res, next) => {
+  console.log("In another middleware!");
+  res.send('<h1>Hellow from Express</h1>') //better than res.write
+
+}));
+
+// const server = http.createServer(app);
+// server.listen(3000);
+
+app.listen(3000);
