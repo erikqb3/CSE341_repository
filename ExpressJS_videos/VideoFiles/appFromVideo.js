39fd4e4bda62f4f55652c31f88b3d9d6e77bdd90
erikqb3 <erikqb3@gmail.com>
2022-01-27 10:29:05 -0700

    change

--- ExpressJS_videos/VideoFiles/appFromVideo.js
+++ ExpressJS_videos/VideoFiles/appFromVideo.js
@@ -30,35 +30,35 @@ app.use(errorController.get404);
 
 // app.listen(3000);
 
-//TEAM ACTIVITY STUFF
-const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+// //TEAM ACTIVITY STUFF
+// const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 
-const corsOptions = {
-    origin: "https://<your_app_name>.herokuapp.com/",
-    optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: "https://<your_app_name>.herokuapp.com/",
+//     optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
 
-const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    family: 4
-};
+// const options = {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     family: 4
+// };
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+// const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
                         
-mongoose.connect(
-    MONGODB_URL, options
-  )
-  .then(result => {
- // This should be your user handling code implement following the course videos
-    app.listen(PORT);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// mongoose.connect(
+//     MONGODB_URL, options
+//   )
+//   .then(result => {
+//  // This should be your user handling code implement following the course videos
+//     app.listen(PORT);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
 
 
